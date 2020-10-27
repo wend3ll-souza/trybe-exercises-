e52e50de544d948277f5eef3b26c78b546f795e1
wend3ll-souza <wend3llsouza@gmail.com>
2020-10-27 15:58:05 -0300

    :checkered_flag: exercise-2 somando os elementos de um array

---
+++ exercises/bloco.4/introducao-a-javascript-e-logica-de-programacao_2/exercises/exercise-2/script.js
@@ -0,0 +1,23 @@
+
+/*Usando reduce 'ainda com duvidas'
+let numbersList = [1, 2, 3];
+let total = numbersList.reduce((total, currentElement) => total + currentElement)
+
+console.log(total)*/
+
+//array que tera os elementos somados 
+let array = [1,2,3,4];
+
+//Variavel que irá armazenar os valores a serem somados 
+let sum = 0;
+
+// repetição, para cada posição do array, 
+for (let i = 0; i < array.length; i+= 1) {
+    //estrutura a ser repetida a cada valor
+  sum += array[i];
+}
+
+//imprimindo o valor da soma 
+console.log(sum)
+
+
