40b0bc16e3dccaee48e2d1dbf4698a611ae397f6
wend3ll-souza <wend3llsouza@gmail.com>
2020-10-27 16:08:40 -0300

    :checkered_flag: exercise-3_4 Media dos elementos de um Array. Indicando se é maior que 20

--- exercises/bloco.4/introducao-a-javascript-e-logica-de-programacao_2/exercises/exercise-3_4/script.js
+++ exercises/bloco.4/introducao-a-javascript-e-logica-de-programacao_2/exercises/exercise-3_4/script.js
@@ -1,19 +1,35 @@
+//array que tera os elementos somados 
+let numberList = [1, 2, 3, 4];
 
-let numberList = [131,321,13,32,4,13,1,2,23121,1231];
+//Variavel que irá armazenar os valores a serem somados 
+let total = 0;
+
+// repetição, para cada posição do array, 
+for (i = 0; i < numberList.length; i++) {
+    //estrutura a ser repetida a cada valor
+    total += numberList[i];
+}
+
+/*estudar reduce
 
 let total = numberList.reduce((total, currentElement) => total + currentElement);
+*/
 
+//variavel que armazena o tamanho do arry
 let tamanho = numberList.length;
 
+//variavel que tará a media 
 let media = total / tamanho;
 
+//impremindo valores
 console.log(`Soma: ${total}`)
 console.log(`Tamanho: ${tamanho}`)
 console.log(`Media: ${media}`)
 
 
-if(media >= 20){
+//Se maior que 20
+if (media >= 20) {
     console.log('valor maior que 20');
-}else{
+} else {
     console.log('valor menor que 20');
 }
