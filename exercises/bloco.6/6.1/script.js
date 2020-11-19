0db863e224309bd4a34c8d0f0432891c1057909f
wend3ll-souza <wend3llsouza@gmail.com>
2020-11-19 12:32:01 -0300

    formulario pronto. Estudando como salvar informações em uma Div

--- exercises/bloco.6/6.1/script.js
+++ exercises/bloco.6/6.1/script.js
@@ -1,67 +1,81 @@
 const estados = ['Acre', 'Alagoas', 'Amapá',
 
-'Amazonas',
+    'Amazonas',
 
-'Bahia',
+    'Bahia',
 
-'Ceará',
+    'Ceará',
 
-'Espírito Santo',
+    'Espírito Santo',
 
-'Goiás',
+    'Goiás',
 
-'Maranhão',
+    'Maranhão',
 
-'Mato Grosso',
+    'Mato Grosso',
 
-'Mato Grosso do Sul',
+    'Mato Grosso do Sul',
 
-'Minas Gerais',
+    'Minas Gerais',
 
-'Pará',
+    'Pará',
 
-'Paraíba',
+    'Paraíba',
 
-'Paraná',
+    'Paraná',
 
-'Pernambuco',
+    'Pernambuco',
 
-'Piauí',
+    'Piauí',
 
-'Rio de Janeiro',
+    'Rio de Janeiro',
 
-'Rio Grande do Norte',
+    'Rio Grande do Norte',
 
-'Rio Grande do Sul',
+    'Rio Grande do Sul',
 
-'Rondônia',
+    'Rondônia',
 
-'Roraima',
+    'Roraima',
 
-'Santa Catarina',
+    'Santa Catarina',
 
-'São Paulo',
+    'São Paulo',
 
-'Sergipe',
+    'Sergipe',
 
-'Tocantins',
+    'Tocantins',
 
-'Distrito Federal'];
-
-
-for (key in estados){
+    'Distrito Federal'
+];
+let dataDeinicio = document.querySelector('#date');
+for (key in estados) {
     let status = document.createElement('option');
     status.innerText = estados[key];
     document.querySelector('#status').appendChild(status);
-    
-}
+};
 
-
-document.addEventListener('click', function(event) {
-    if(event.target.className == 'buttonS'){
-        console.log('oi')
-    }
-})
-
-
-
+addEventListener('click', (event) => {
+    if (event.target.id === 'submit') {
+        event.preventDefault();
+        let form = document.body.querySelector('.form');
+        const required = document.querySelectorAll("[required]");
+        const date = dataDeinicio.value.split('/');
+        const day = parseInt(date[0]);
+        const month = parseInt(date[1]);
+        const year = parseInt(date[2]);
+        let errors = '';
+        for (key in required) {
+            let input = required[key];
+            if (input.required) {
+                if (input.value === "") {
+                    errors += `O campo ${input.name} é obrigatório! \n`;
+                };
+            };
+        };
+        if (!(day > 0 || day <= 31 || month > 0 || month <= 12 || year > 0)) {
+            errors += 'Insira uma data com o formato dd/mm/aaaa.';
+        };
+        alert(errors);
+    };
+});
