6cfcede319284018a94aa7b008952126e7ce62ec
wend3ll-souza <wend3llsouza@gmail.com>
2020-11-23 21:31:08 -0300

    :package: Exercicios Parte 1 Pronto

---
+++ exercises/bloco.7/7.1/script.js
@@ -0,0 +1,39 @@
+//Parte 1
+
+//1
+
+const arrow = (escopo) => { 
+    if (escopo === true) { 
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+      console.log(ifScope);
+    } else {
+      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+    console.log(`${ifScope} o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
+  }
+
+//2
+
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+  // Seu código aqui.
+
+  console.log(`${oddsAndEvens.sort((a, b)=>a - b)}`);
+
+  
+//Parte 2
+
+//1
+
+  function produto(param) {
+    let result = param;
+      for(key = 1; key < param; key++){
+        result = result * key;
+      }
+      return result;
+  }
+
+  console.log(produto(4));
+
---
+++ exercises/bloco.7/7.1/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+  function produto(param) {
+      for(key = 1; key < param; key++){
+        param *= key;
+      }
+      return result;
+  }
+
+  console.log(produto(4));
