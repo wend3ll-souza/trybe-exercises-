7b0a7c6c0582c08fc62d65d2b91437e93e9d4bbc
wend3ll-souza <wend3llsouza@gmail.com>
2020-12-11 23:55:28 -0300

    :package: 8.2 exercicio 2 pronto

--- exercises/bloco.8/exercises/8.2/exercises/exercises2.js
+++ exercises/bloco.8/exercises/8.2/exercises/exercises2.js
@@ -91,7 +91,12 @@ const expectedResult = [
 ];
 
 function nameAndAge() {
-  // escreva seu código aqui
+  return books
+    .map(book => ({
+      author: book.author.name,
+      age: book.releaseYear - book.author.birthYear
+    }))
+    .sort((obj1, obj2) => obj1.age - obj2.age)
 }
 
 assert.deepStrictEqual(nameAndAge(), expectedResult);
