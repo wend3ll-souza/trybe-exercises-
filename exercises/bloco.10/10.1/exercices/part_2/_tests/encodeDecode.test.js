b124c23fa0fe9b91469aa9a87f1d2b22f6188d02
wend3ll-souza <wend3llsouza@gmail.com>
2021-01-07 11:56:23 -0300

    :package: 10.2 part_2 exercicio 1.2

--- exercises/bloco.10/10.1/exercices/part_2/_tests/encodeDecode.test.js
+++ exercises/bloco.10/10.1/exercices/part_2/_tests/encodeDecode.test.js
@@ -7,3 +7,9 @@ describe('1 - testando se são funções', () => {
         expect(typeof data.decode).toBe('function');
     })
 })
+
+describe('2 - testando se enconde vogais retorna seus respectivos númeors', () => {
+    it('testando encode', () => {
+        expect(data.encode(`a, e, i, o, u`)).toBe(`1, 2, 3, 4, 5`);
+    })
+})
