20542c411e31d64cf89eb2d29ecb22ae493a76c1
wend3ll-souza <wend3llsouza@gmail.com>
2021-01-13 13:32:44 -0300

    :package: 11.2 aprendendo class, props e a criar componentes

--- exercises/bloco.11/11.2/praticando/test_react/src/App.js
+++ exercises/bloco.11/11.2/praticando/test_react/src/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import Greeting from './greeting';
+import Image from './img';
+function App() {
+  return (
+    <div>
+      <Image source="https://source.unsplash.com/random" alternativeText="imagem aleatoria"/>
+      <Greeting name="Wend3ll" last="Bob"/>
+    </div> 
+  )
 }
 
 export default App;
